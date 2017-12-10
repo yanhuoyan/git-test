1bd0663119922505bf6f02afbd8638eac5032011
yanhuoyan <wangrenhaomit@163.com.cn>
2017-12-10 16:01:35 +0800

    更新一下

--- src/com/mryx/giit/GitTest.java
+++ src/com/mryx/giit/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("this is git test");
-		System.out.println("git update this");
+		System.out.println("set eclipse this");
 		System.out.println("git update this2");
 	}
 }
