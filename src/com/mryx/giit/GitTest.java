55893bdc005e508f60516fbb039961b6261dd7ff
yanhuoyan <wangrenhaomit@163.com.cn>
2017-12-10 16:17:01 +0800

    eclipse update

--- src/com/mryx/giit/GitTest.java
+++ src/com/mryx/giit/GitTest.java
@@ -3,8 +3,8 @@ package com.mryx.giit;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("this is git test");
-		System.out.println("set eclipse this");
+		System.out.println("this test");
+		System.out.println("set this");
 		System.out.println("git update this2");
 	}
 }
