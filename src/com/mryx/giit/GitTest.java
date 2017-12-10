c25d5e9f10e42b2ef3505ab4e94a9c535ab1df78
yanhuoyan <wangrenhaomit@163.com.cn>
2017-12-10 16:37:38 +0800

    update branch

--- src/com/mryx/giit/GitTest.java
+++ src/com/mryx/giit/GitTest.java
@@ -3,7 +3,7 @@ package com.mryx.giit;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("this eclipse exception update git is git test");
+		System.out.println("this eclipse exception update branch update git is git test");
 		System.out.println("set eclipse exception update git eclipse this");
 		System.out.println("git update this2");
 	}
